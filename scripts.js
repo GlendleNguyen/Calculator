29bd074e8004de51111bd9bc3da16d6b9cd8dea2
GlendleNguyen <glendle.nguyen@gmail.com>
2021-12-18 21:27:20 +0800

    fixed division by 0 syntax

--- scripts.js
+++ scripts.js
@@ -116,7 +116,7 @@ function multiply(a, b) {
 function divide(a, b) {
     if (b == 0) {
         console.log("dividing by 0")
-        clear
+        clear()
     } else {
         return a / b
     }
